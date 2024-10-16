b91817a63f10fce118854e5f1432928e0ae5df3b
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-10-16 15:34:52 -0400

    feat: create account controller and zod validation

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,54 @@
+import {
+  Body,
+  ConflictException,
+  Controller,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  cpf: z.string().length(11),
+  password: z.string(),
+  role: z.enum(['ADMIN', 'COURIER']),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {
+    //
+  }
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, cpf, password, role } = body
+
+    const userWithSameCpf = await this.prisma.user.findUnique({
+      where: {
+        cpf,
+      },
+    })
+
+    if (userWithSameCpf) {
+      throw new ConflictException('User with same CPF already exists.')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        cpf,
+        password: hashedPassword,
+        role,
+      },
+    })
+  }
+}
